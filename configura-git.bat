726419b9a383e917ae1f968ff76ac15e9242e97a
Eduardo marinho <eduardomarinho0506@gmail.com>
2024-08-09 21:43:35 -0300

    Primeira pagina web

---
+++ configura-git.bat
@@ -0,0 +1,12 @@
+echo ON
+echo --- Configurando usuario GitHub ----
+echo
+echo
+git config --global user.name "Eduardo marinho"
+git config --global user.email "eduardomarinho0506@gmail.com"
+echo ---mostrando a configuração atual do GitHub no computador
+git config -- list
+echo
+echo --fim ---
+
+pause
